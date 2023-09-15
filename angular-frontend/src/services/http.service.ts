e9b6fdaeb0e0ee8f7588db502ccb50cbc3b6c35f
username <wecp-dummy@gmail.com>
2023-09-15 08:25:52 +0000

    angular changed

--- angular-frontend/src/services/http.service.ts
+++ angular-frontend/src/services/http.service.ts
@@ -6,146 +6,149 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class HttpService {
-  public serverName = window.location.origin.replace("3000", "5000") + "/api/";
-  constructor(private http: HttpClient) {}
+  public serverName=window.location.origin.replace("3000", "5000") + "/api/";
+  constructor(private http: HttpClient) {
 
-   // TODO: Get All Table List
+   }
+   //Get All Table List
    GetTables():Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+    return this.http.get(this.serverName+'Tables',{headers:headers});
+  
+  
   }
-
-  // TODO: get Table status empty or occupied
+  //get Table status empty or occupied
   GetTableStatus():Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+  return this.http.get(this.serverName+'GetTableStatus',{headers:headers});
   }
-  
-  // TODO: get recent reservations
+  //get recent reservations
   GetRecentReservation():Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.get(this.serverName+'GetRecentReservations',{headers:headers});
   }
   
-  // TODO: Save table in Table 
+  //Save table in Table 
   SaveTable(details:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.post(this.serverName+'AddTable',details,{headers:headers});
   }
-
-  // TODO: Delete table from DB 
+  //Delete table from DB 
   DeleteTable(id:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null
+   return this.http.delete(this.serverName+'Delete?id='+id,{headers:headers});
   }
-
-  // TODO: Get all menu from DB
+  //Get all menu from DB
   GetMenu():Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.get(this.serverName+'MenuItems',{headers:headers});
   }
 
-  // TODO: Add new menu in DB through menucontroller
+  //Add new menu in DB through menucontroller
   AddMenu(details:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.post(this.serverName+'AddMenu',details,{headers:headers});
   }
-
-  // TODO: Delete menu in db DB through menucontroller
-  DeleteMenu(id:any):Observable<any> {  
+  //Delete menu in db DB through menucontroller
+  DeleteMenu(id:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.delete(this.serverName+'DeleteMenu?id='+id,{headers:headers});
   }
-
-  // TODO: add new reservation in DB through reservationcontroller
+  //add new reservation in DB through reservationcontroller
   AddReservations(details:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.post(this.serverName+'AddReservations',details,{headers:headers});
   }
-
-  // TODO: get reservation with table from db through reservationcontroller
+  //get reservation with table from db through reservationcontroller
   GetReservationWithTable(id:any):Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.get(this.serverName+'GetReservationWithTable?Id='+id,{headers:headers});
   }
-
-  // TODO: delete reservation table item from db through reservationcontroller
+  //delte reservation table item from db through reservationcontroller
   DeleteReservation(id:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.delete(this.serverName+'DeleteReservation?id='+id,{headers:headers});
   }
-
-  // TODO: Cancel Reservation from db through reservationcontroller
+  //Cancel Reservation from db through reservationcontroller
   CancelReservation(id:any,userno:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+  return this.http.delete(this.serverName+'CancelReservation?id='+id+'&userno='+userno,{headers:headers});
   }
-
-  // TODO: Cancel reservation from db  through reservationcontroller
+  //Cancel reservation from db  through reservationcontroller
   CancelReservationAdmin(id:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.delete(this.serverName+'CancelReservationAdmin?id='+id,{headers:headers});
   }
-
-  // TODO: Add new order in db through ordercontroller
+  //Add new order in db through ordercontroller
   AddOrders(details:any):Observable<any> {
+    debugger;
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.post(this.serverName+'SaveOrders',details,{headers:headers});
   }
-
-  // TODO: get All orders from db by ordercontroller
+  //get All orders from db by ordercontroller
   Orders():Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.get(this.serverName+'Orders',{headers:headers});
   }
-
-  // TODO: get order by id from db by ordercontroller
+  //get order by id from db by ordercontroller
   getOrdersById(id:any):Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+  return this.http.get(this.serverName+'OrderById?id='+id,{headers:headers});
   }
-
-  // TODO: delete order from db by ordercontroller
+  //delete order from db by ordercontroller
   DeleteOrder(id:any):Observable<any> {
+  
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+  return this.http.delete(this.serverName+'DeleteOrder?id='+id,{headers:headers});
   }
-
-  // TODO: update order in db by ordercontroller
+  //update order in db by ordercontroller
   UpdateOrders(details:any):Observable<any> {
+    debugger;
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.post(this.serverName+'UpdateOrders',details,{headers:headers});
   }
-
-  // TODO: login user by login controller
+  //login user by login controller
   Login(details:any):Observable<any> {
+    
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
+   return this.http.post(this.serverName+'Login/LoginNow',details,{headers:headers});
   }
-
-  // TODO: register new user by login controller
+  //register new user by login controller
   registerUser(details:any):Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return null;
-  }  
+   return this.http.post(this.serverName+'Login/RegisterUser',details,{headers:headers});
+  }
+ 
+  
+  
 }
+
+
